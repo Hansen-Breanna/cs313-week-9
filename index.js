f034dd6692475cca7274f67f8a305b350e6e4ea5
Breanna Hansen <hansbrea@hotmail.com>
2021-03-02 23:18:49 -0700

    week 9 prove

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
+app.use(express.urlencoded({
+  extended: true
+}))
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/calculator', (req, res) => res.render('pages/calculator'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
